34f7872b00d99acc1de52b7a174d42940fa4ffbe
Treethon <ninomack5@yahoo.com>
2026-05-13 00:21:51 -0400

    commit of initial event and adventurer class

---
+++ adventurer.py
@@ -0,0 +1,15 @@
+class Adventurer:
+    def __init__(self):
+        pass
+
+class Barbarian(Adventurer):
+    def __init__(self):
+        pass
+
+class Sorceror(Adventurer):
+    def __init__(self):
+        pass
+
+class Scoundrel(Adventurer):
+    def __init__(self):
+        pass
---
+++ event.py
@@ -0,0 +1,20 @@
+class Event:
+    def __init__(self):
+        pass
+
+    def greet():
+        pass
+
+class NormalEvent(Event):
+    def __init__(self):
+        pass
+
+    def greet():
+        pass
+
+class BossEvent(Event):
+    def __init__(self):
+        pass
+    
+    def greet():
+        pass
